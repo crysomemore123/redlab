2a9b9d6728dfced13e916d5e55c8c6efd27a9664
crysomemore123 <revaz.gachechiladze.1@btu.edu.ge>
2025-11-19 20:16:11 +0400

    image load speed increase

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    // This enables AVIF support (faster than WebP)
+    formats: ['image/avif', 'image/webp'],
+  },
   // No rewrites needed since we've removed all dropdown sections
 };
 

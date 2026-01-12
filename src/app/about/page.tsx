83ccc5356badf6cb5e9756a00adb667324293dbc
crysomemore123 <revaz.gachechiladze.1@btu.edu.ge>
2026-01-12 23:22:42 +0400

    fix

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -47,15 +47,7 @@ export default function About() {
         <div className={styles.profileInfo}>
           <h2>IRINA GACHECHILADZE | Artistic Director and Founder</h2>
           <p>
-            Is a New York-based stage and opera director and choreographer. Recent productions include the world
-            premieres of Contemporary Georgian play <em>Stalin Lab, The Seagull</em> (TheatreLab), <em>The Brashworrier&apos;s Apprentice, Nicholas and
-            Alexandra</em> and <em>Night About the City</em> (all with Amos at the Oracle Theater). <em>Hamlet A Version</em>, an adaptation of <em>Hamlet</em> in English and
-            its anthology by Pavel at Westminster Choir College. International productions include Mozart&apos;s <em>Don
-            Giovanni</em> co-produced with The Georgian State Opera, <em>Twilight After the lover</em> in co-op by Alexander Pushkin (Marjanishvili
-            Theater, Tbilisi, Georgia), and Glen Cario Menotti&apos;s <em>The Telephone</em> which was produced in her native Tbilisi,
-            Georgia. She holds a master&apos;s degree in stage directing from the Russian Academy of Performing Arts (GITIS, Moscow). She
-            is a founding member of the League of Professional Theater and has extensive film and visual arts credentials. Her films <em>As
-            Apple</em> and <em>Stigma</em> received awards at the Kinoshock Film Festival in Anapa, Russia.
+            Is a New York-based stage and opera director and choreographer. Recent productions include the world premiere of Boris Akunin's Hamlet. A Version (Red Lab), The Seagull (TheatreLab); The Breadbaker’s Apprentice, Nicholas and Alexandra, and High Above the City (all with Amios at the Kraine Theatre); and the operas Suor Angelica by Puccini and L’enfant et les sortilèges by Ravel at Westminter Choir College. International productions include Mozart’s Don Giovanni (National Armenian Opera in Yerevan); Eugene Onegin, after the novel in verse by Alexander Pushkin (Marjanishvili Theater, Tbilisi, Georgia), and Gian Carlo Menotti’s The Telephone (Salon Opera, London). A native of the Republic of Georgia, she holds a master’s degree in stage directing from the Russian Academy of Performing Arts (GITIS, Moscow). She is a member of the Lincoln Center Theater Directors Lab, and also has extensive film and visual arts credits. Her films An Apple and Stigma received awards at the Kinoshok Film Festival in Anapa, Russia.
           </p>
         </div>
       </div>
